8242be989bef9168b6b33aba739416a8e5d9d3ec
gagarinn <viktor_burmaka@ukr.net>
2015-12-28 23:41:44 +0300

    added ch to 12 DatePicker. TODO time picker

--- criminalintent/src/main/java/cps/ru/criminalintent/CrimeListFragment.java
+++ criminalintent/src/main/java/cps/ru/criminalintent/CrimeListFragment.java
@@ -37,7 +37,8 @@ public class CrimeListFragment extends ListFragment {
 
         Crime c = ((CrimeAdapter)getListAdapter()).getItem(position);
 
-        Intent i = new Intent(getActivity(), CrimeActivity.class);
+
+        Intent i = new Intent(getActivity(), CrimePagerActivity.class);
         i.putExtra(CrimeFragment.EXTRA_CRIME_ID, c.getId());
         startActivity(i);
     }
